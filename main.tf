b7c448a5a548f11ea13f16a037162470b5c5890a
karuppiah.jay <karuppiah.jay@royalmail.onmicrosoft.com>
2025-08-19 15:02:36 +0100

    s3 bucket ownership and public access block resourc added

--- main.tf
+++ main.tf
@@ -2,3 +2,20 @@ resource "aws_s3_bucket" "mybucket" {
   bucket = var.bucketName
 
 }
+
+resource "aws_s3_bucket_ownership_controls" "mybucket_ownership_controls" {
+  bucket = aws_s3_bucket.mybucket.id
+
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "mybucket_public_access_block" {
+  bucket = aws_s3_bucket.mybucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
