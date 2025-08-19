9a640d572e4f11e2aca90b53a5628f67001f443d
karuppiah.jay <karuppiah.jay@royalmail.onmicrosoft.com>
2025-08-19 17:18:51 +0100

    index and error html file added and css object block removed from main

--- main.tf
+++ main.tf
@@ -29,3 +29,33 @@ resource "aws_s3_bucket_acl" "mybucket_acl" {
   bucket = aws_s3_bucket.mybucket.id
   acl    = "public-read"
 }
+
+resource "aws_s3_object" "index" {
+  bucket = aws_s3_bucket.mybucket.id
+  key    = "index.html"
+  source = "index.html"
+  acl   = "public-read"
+  content_type = "text/html"
+  
+}
+resource "aws_s3_object" "error" {
+  bucket = aws_s3_bucket.mybucket.id
+  key    = "error.html"
+  source = "error.html"
+  acl   = "public-read"
+  content_type = "text/html"
+}
+
+resource "aws_s3_bucket_website_configuration" "mybucket_website" {
+  bucket = aws_s3_bucket.mybucket.id
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "error.html"
+  }
+
+  depends_on = [ aws_s3_bucket_acl.mybucket_acl ]
+}
