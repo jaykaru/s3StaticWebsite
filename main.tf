da7316bfe7151adb919f9c8e5622bfda73bf35d8
karuppiah.jay <karuppiah.jay@royalmail.onmicrosoft.com>
2025-08-19 15:23:40 +0100

    bucket acl resource added

--- main.tf
+++ main.tf
@@ -19,3 +19,13 @@ resource "aws_s3_bucket_public_access_block" "mybucket_public_access_block" {
   ignore_public_acls      = false
   restrict_public_buckets = false
 }
+
+resource "aws_s3_bucket_acl" "mybucket_acl" {
+  depends_on = [
+    aws_s3_bucket_ownership_controls.mybucket_ownership_controls,
+    aws_s3_bucket_public_access_block.mybucket_public_access_block,
+  ]
+
+  bucket = aws_s3_bucket.mybucket.id
+  acl    = "public-read"
+}
